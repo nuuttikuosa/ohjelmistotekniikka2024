7ce87250097eb28a68d1f4d9d4ec01824195f57b
Nuutti Kuosa <nuutti.kuosa@helsinki.fi>
2024-03-22 01:00:20 +0200

    unittests

--- viikko2/unicafe/src/tests/maksukortti_test.py
+++ viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_saldo_kasvaa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 20.0)
+
+    def test_saldo_vähenee_oikein(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5.0)
+
+    def test_ei_vähene_jos_ei_rahaa(self):
+        self.maksukortti.ota_rahaa(1500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_saldo_on_rahaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+
+    def test_saldo_on_rahaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1500), False)
