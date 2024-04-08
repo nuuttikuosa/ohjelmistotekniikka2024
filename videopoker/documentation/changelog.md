58dbefdae443232cdfbda5b099a709f5a2c0e18c
Nuutti Kuosa <nuutti.kuosa@helsinki.fi>
2024-04-09 00:37:21 +0300

    viikon 3 muutoksia ja lisäyksiä

---
+++ videopoker/documentation/changelog.md
@@ -0,0 +1,7 @@
+## Viikko 3
+
+- Tehty seuraavat luokat: PlayingCard, Dealer, Deck, PokerHand, PokerHandEvaluator ja 
+  VideoPokerService.
+- lisätty VideoPokerService-luokka, joka vastaa sovelluslogiikan koodista
+- tehty yksinkertainen tekstipohjainen käyttöliittymä tiedostoon index.py
+- kirjoitettu yksikötestit luokalle card, tämän luokan osalta testikarravuus 100%.
--- videopoker/tasks.py
+++ videopoker/tasks.py
@@ -3,19 +3,19 @@ import subprocess
 from sys import platform
 
 @task
-def foo(ctx):
-   print("bar")
-
-@task
 def start(ctx):
    ctx.run("python3 src/index.py", pty=True)
 
 @task
 def coverage(ctx):
-   ctx.run("coverage run --branch -m pytest src/", pty=True)
+   ctx.run("coverage run --branch -m pytest", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
    ctx.run("coverage html", pty=True)
    if platform != "win32":
       subprocess.Popen(["xdg-open", "htmlcov/index.html"])
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
