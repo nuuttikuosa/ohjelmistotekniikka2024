2ead6094fdacd7d55a990c5b2c6193672d23bbcc
Nuutti Kuosa <nuutti.kuosa@helsinki.fi>
2024-04-22 17:28:30 +0300

    parannttu unit test kattavuutta

---
+++ videopoker/src/tests/user_test.py
@@ -0,0 +1,10 @@
+import unittest
+from entities.user import User
+
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.user = User("Pelaaja", 1000)
+
+    def test_creation_and_string_conversion (self):
+        self.assertEqual(str(self.user), "Pelaaja: 1000")
